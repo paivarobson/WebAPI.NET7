5d15c075b3da0cb685edfc9bc9891fda2d92e699
Robson Paiva <paivawork@hotmail.com>
2023-06-21 18:04:16 -0300

    Camada de dados Employee

---
+++ WebAPI.NET7/Infrastructure/ConnectionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.NET7.Model;
+
+namespace WebAPI.NET7.Infrastructure
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql(
+                "Server=localhost;" +
+                "Port=5432;" +
+                "Database=Empresa;" +
+                "User Id=postgres;" +
+                "Password=123456;");
+    }
+}
+
---
+++ WebAPI.NET7/Infrastructure/EmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using WebAPI.NET7.Model;
+
+namespace WebAPI.NET7.Infrastructure
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ConnectionContext _context = new ConnectionContext();
+
+        void IEmployeeRepository.Add(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+        }
+
+        List<Employee> IEmployeeRepository.Get()
+        {
+            return _context.Employees.ToList();
+        }
+    }
+}
+
