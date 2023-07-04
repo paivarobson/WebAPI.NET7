4440acc133e57052d8bcd9249d1a0dd5593f8523
Robson Paiva <paivawork@hotmail.com>
2023-07-04 13:40:03 -0300

    Adicionada nova entidade Company e usando Entity Framework Core para adicionar as tabelas no BD - Code First

---
+++ WebAPI.NET7/Domain/Model/CompanyAggregate/Company.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.NET7.Domain.Model.CompanyAggregate
+{
+    [Table("company")]
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
--- WebAPI.NET7/Infrastructure/ConnectionContext.cs
+++ WebAPI.NET7/Infrastructure/ConnectionContext.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WebAPI.NET7.Domain.Model;
+using WebAPI.NET7.Domain.Model.CompanyAggregate;
 
 namespace WebAPI.NET7.Infrastructure
 {
     public class ConnectionContext : DbContext
     {
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Company> Company { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql(
